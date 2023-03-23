0f8b27eef2e724334fa906615fc04469c6049abb
rcbcastillo <rcbcastillo@gmail.com>
2023-03-23 12:41:48 +0000

    Add methods to add and delete vehicles

--- src/main/java/DemoGenerics.java
+++ src/main/java/DemoGenerics.java
@@ -3,13 +3,15 @@ import com.qa.garage.vehicle.Car;
 public class DemoGenerics {
 
 	public static void main(String[] args) {
-		// instance of Generic
-		
+		// instance of Generic		
 		GenericVehicleContainer<Car> formula1 = new GenericVehicleContainer<>();
 		
 		formula1.setVehicle(new Car(4, "green"));
 		Car carFormula1 = formula1.getVehicle();
 		System.out.println(carFormula1);
+		
+		// adding vehicles
+		formula1.addVehicle(carFormula1);
 
 	}
 
--- src/main/java/GenericVehicleContainer.java
+++ src/main/java/GenericVehicleContainer.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import com.qa.garage.vehicle.Vehicle;
 
+import vehicles.VehicleNotFoundException;
+
 public class GenericVehicleContainer <T extends Vehicle> {
+	
+	private List<T> vehicles = new ArrayList<>();
 	
 	private T vehicle;
 	
@@ -10,6 +17,22 @@ public class GenericVehicleContainer <T extends Vehicle> {
 	
 	public void setVehicle(T vehicle) {
 		this.vehicle = vehicle;
+	}
+	
+	public boolean addVehicle(T vehicle) {
+		return this.vehicles.add(vehicle);
+	}
+	
+	public boolean remove(int id) throws VehicleNotFoundException {
+		
+		for (Vehicle vehicle : this.vehicles) {
+			if (vehicle.getId() == id) {
+				this.vehicles.remove(vehicle);
+				return true;
+			}
+			throw new VehicleNotFoundException("Id not found");
+		}
+		return false;
 	}
 
 }
